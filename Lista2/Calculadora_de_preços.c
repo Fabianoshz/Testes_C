d71eaeafbd31e29be4e25f381f4ea7d880ae44b0
Fabiano Honorato <fabianoshz@gmail.com>
2016-09-25 13:41:08 -0300

    Organização --'

--- Lista2/Calculadora_de_preços.c
+++ Lista2/Calculadora_de_preços.c
@@ -5,15 +5,15 @@ int main() {
 	float p;
 
 	printf("Por favor, digite o preço do produto:");
-	scanf("%f", &p); // Guardar o valor recebido na variável p.
+	scanf("%f", &p); // Guardar o valor recebido na variÃ¡vel p.
 
-	float pagvista = p - (p * 0.1);
+	float pagvista = p * 0.9;
 	float pagparcelado5 = p / 5;
-	float pagparcelado20 = ((p * 0.2) + p ) / 20;
+	float pagparcelado10 = (p * 1.2) / 10;
 
 	printf("Pagamento a vista: %f \n", pagvista);
 	printf("Pagamento parcelado em 5x sem juros: %f \n", pagparcelado5);
-	printf("Pagamento parcelado em 20x com juros: %f \n", pagparcelado20);
+	printf("Pagamento parcelado em 10x com juros: %f \n", pagparcelado10);
 
 	return 0;
 
